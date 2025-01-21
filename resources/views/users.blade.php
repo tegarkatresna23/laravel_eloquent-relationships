603add04034f2db99fc8110f8c6e084aa0f67395
tegarkatresna23 <tegarkatresna@gmail.com>
2025-01-21 09:48:29 +0700

    menampilkan data role di data user

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,9 +1,10 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Eloquent Relationships : Relasi One to One</title>
+    <title>Eloquent Relationships : Relasi One to One & Many To Many</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
         crossorigin="anonymous">
 </head>
@@ -12,12 +13,13 @@
         <div class="card mt-5">
             <div class="card-body">
                 <h3 class="text-center"><a href="https://santrikoding.com">www.santrikoding.com</a></h3>
-                <h5 class="text-center my-4">Laravel Eloquent Relationship : One To One</h5>
+                <h5 class="text-center my-4">Laravel Eloquent Relationship : One To One & Many To Many</h5>
                 <table class="table table-bordered table-striped">
                     <thead>
                         <tr>
                             <th>Nama User</th>
                             <th>Nomor Telepon</th>
+                            <th>Roles</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -25,6 +27,11 @@
                         <tr>
                             <td>{{ $user->name }}</td>
                             <td>{{ $user->phone->phone }}</td>
+                            <td>
+                                @foreach ($user->roles()->get() as $role)
+                                <button class="btn btn-sm btn-primary me-2">{{ $role->name }}</button>
+                                @endforeach
+                            </td>
                         </tr>
                         @endforeach
                     </tbody>
